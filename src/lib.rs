922391112e49fa8077b20d89a1d3b44d276acb0f
fran <fran.sl@atlascity.io>
2019-03-27 17:22:28 -0600

    added lib base files

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use std::os::raw::c_char;
+use std::ffi::CString;
+
+extern crate curve25519_dalek;
+use curve25519_dalek::ristretto::CompressedRistretto;
+use curve25519_dalek::scalar::Scalar;
+
+use std::mem::transmute;
+
+#[repr(C)]
+pub struct Buffer {
+    data: *mut u8,
+    len: usize,
+}
+
+
+#[repr(C)]
+pub struct FFIBytes64 {    
+    bytes: [u8; 64]
+}
+
+pub extern fn free_buf(buf: Buffer) {
+    let s = unsafe { std::slice::from_raw_parts_mut(buf.data, buf.len) };
+    let s = s.as_mut_ptr();
+    unsafe {
+        Box::from_raw(s);
+    }
+}
+
+
+
+#[no_mangle]
+pub extern "C" fn ct_sign(key: *const [u8; 32]) -> FFIBytes64 {
+    let mut byte_array = [0u8; 64];
+    thread_rng().fill(&mut byte_array[..]);
+    println!("array occupies {} bytes", mem::size_of_val(&byte_array));
+    FFIBytes64 {
+        bytes: byte_array
+    }
+}
+
+
+
+
+
+
