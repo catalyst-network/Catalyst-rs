94d6c50c144c6112c43e2a92d747b488b2622de5
fran <fran.sl@atlascity.io>
2019-03-28 12:48:03 -0600

    key generation working using dalek-ed25519

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,13 @@
-use std::os::raw::c_char;
-use std::ffi::CString;
-use rand::{thread_rng, Rng};
-use std::mem;
-
-extern crate curve25519_dalek;
-use curve25519_dalek::ristretto::CompressedRistretto;
-use curve25519_dalek::scalar::Scalar;
-
-#[repr(C)]
-pub struct Buffer {
-    data: *mut u8,
-    len: usize,
-}
-
-
-#[repr(C)]
-pub struct FFIBytes64 {    
-    bytes: [u8; 64]
-}
-
-pub extern fn free_buf(buf: Buffer) {
-    let s = unsafe { std::slice::from_raw_parts_mut(buf.data, buf.len) };
-    let s = s.as_mut_ptr();
-    unsafe {
-        Box::from_raw(s);
-    }
-}
-
+use rand::thread_rng;
+extern crate ed25519_dalek;
+use ed25519_dalek::SecretKey;
 
 
 #[no_mangle]
-pub extern "C" fn key_gen(out_key: &mut [u8;32]) {
-    thread_rng().fill(out_key);
-    //println!("array occupies {} bytes", mem::size_of_val(&byte_array));
+pub extern "C" fn generate_key(out_key: &mut [u8;32]) {
+    let mut csprng = thread_rng();
+    let secret_key: SecretKey = SecretKey::generate(&mut csprng);
+    out_key.copy_from_slice(&secret_key.to_bytes());
 }
 
 
