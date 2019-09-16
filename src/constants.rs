ceebbfa6b3852197f222ea4a958da614b6f95b21
franssl <46971650+franssl@users.noreply.github.com>
2019-09-16 14:21:14 +0100

    Make ffi module public (#28)

--- src/constants.rs
+++ src/constants.rs
@@ -20,16 +20,20 @@
 /// The length of a ed25519 `Signature`, in bytes.
 pub const SIGNATURE_LENGTH: usize = 64;
 
-/// The length of a ed25519 `PrivateKey`, in bytes.
+/// The length of a ed25519ph `PrivateKey`, in bytes.
 pub const PRIVATE_KEY_LENGTH: usize = 32;
 
-/// The length of an ed25519 `PublicKey`, in bytes.
+/// The length of an ed25519ph `PublicKey`, in bytes.
 pub const PUBLIC_KEY_LENGTH: usize = 32;
 
+/// The max of the ed25519ph context, in bytes.
 pub const CONTEXT_MAX_LENGTH: usize = 255;
 
+/// Signifies an issue with the signature or public key.
 pub const SIGNATURE_ERROR : i32 = 101;
 
+/// The provided context exceeds the maximum allowed length.
 pub const CONTEXT_LENGTH_ERROR: i32 = 201;
 
+/// An unknown error.
 pub const UNKNOWN_ERROR : i32 = -1;
