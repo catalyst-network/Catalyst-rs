88a217d66cd9a3b1a4ad42a651742c424f0e45ea
fran <fran.sl@atlascity.io>
2019-09-27 14:30:08 +0100

    Change license to MIT

--- src/keys.rs
+++ src/keys.rs
@@ -1,20 +1,3 @@
-// Copyright (c) 2019 Catalyst Network
-//
-// This file is part of Rust.Cryptography.FFI <https://github.com/catalyst-network/catalyst-ffi>
-//
-// Rust.Cryptography.FFI is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// Rust.Cryptography.FFI is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Rust.Cryptography.FFI. If not, see <https://www.gnu.org/licenses/>.
-
 //! ed25519 keys
 
 use rand::thread_rng;
